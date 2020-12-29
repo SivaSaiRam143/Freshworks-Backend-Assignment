5f6e28d8f6cd77d7d480031f469da96fc830a658
SivaSaiRam143 <43082972+SivaSaiRam143@users.noreply.github.com>
2020-12-29 22:22:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,54 @@
 import threading 
-#this is for python 3.0 and above. use import thread for python2.0 versions
 from threading import*
 import time
 
-d={} #'d' is the dictionary in which we store data
+datastore={}
 
-#for create operation 
-#use syntax "create(key_name,value,timeout_value)" timeout is optional you can continue by passing two arguments without timeout
-
-def create(key,value,timeout=0):
-    if key in d:
-        print("error: this key already exists") #error message1
+def create(key,val,timeout=0):
+    if key in datastore:
+        print("ERROR: Key provided is already present in the datastore")
     else:
         if(key.isalpha()):
-            if len(d)<(1024*1020*1024) and value<=(16*1024*1024): #constraints for file size less than 1GB and Jasonobject value less than 16KB 
+            if len(datastore)<(1024*1020*1024) and val<=(16*1024*1024): #checking size of datastore < 1 GB and value  is < 16 KB
                 if timeout==0:
-                    l=[value,timeout]
+                    t=[val,timeout]
                 else:
-                    l=[value,time.time()+timeout]
-                if len(key)<=32: #constraints for input key_name capped at 32chars
-                    d[key]=l
+                    t=[val,time.time()+timeout]
+                if len(key)<=32: #key< 32chars
+                    datastore[key]=t
             else:
-                print("error: Memory limit exceeded!! ")#error message2
+                print("ERROR: Memory Limit Exceeded!! ")#ERROR: Memory Limit Violation
         else:
-            print("error: Invalind key_name!! key_name must contain only alphabets and no special characters or numbers")#error message3
+            print("ERROR: Invalid keyname is entered")
 
-#for read operation
-#use syntax "read(key_name)"
             
 def read(key):
-    if key not in d:
-        print("error: given key does not exist in database. Please enter a valid key") #error message4
+    if key not in datastore:
+        print("ERROR: Key not found in datastore")
     else:
-        b=d[key]
-        if b[1]!=0:
-            if time.time()<b[1]: #comparing the present time with expiry time
-                stri=str(key)+":"+str(b[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
-                return stri
+        temp=datastore[key]
+        if temp[1]!=0:
+            if time.time()<temp[1]: 
+                s=str(key)+":"+str(temp[0])
+                return s
             else:
-                print("error: time-to-live of",key,"has expired") #error message5
+                print("ERROR: TTL expired") 
         else:
-            stri=str(key)+":"+str(b[0])
-            return stri
+            s=str(key)+":"+str(temp[0])
+            return s
 
-#for delete operation
-#use syntax "delete(key_name)"
-
+        
 def delete(key):
-    if key not in d:
-        print("error: given key does not exist in database. Please enter a valid key") #error message4
+    if key not in datastore:
+        print("ERROR: Key not found")
     else:
-        b=d[key]
-        if b[1]!=0:
-            if time.time()<b[1]: #comparing the current time with expiry time
-                del d[key]
-                print("key is successfully deleted")
+        temp=datastore[key]
+        if temp[1]!=0:
+            if time.time()<temp[1]:
+                del datastore[key]
+                print("Key is removed")
             else:
-                print("error: time-to-live of",key,"has expired") #error message5
+                print("ERROR: TTL expired")
         else:
-            del d[key]
-            print("key is successfully deleted")
-
-#I have an additional operation of modify in order to change the value of key before its expiry time if provided
-
-#for modify operation 
-#use syntax "modify(key_name,new_value)"
-
-def modify(key,value):
-    b=d[key]
-    if b[1]!=0:
-        if time.time()<b[1]:
-            if key not in d:
-                print("error: given key does not exist in database. Please enter a valid key") #error message6
-            else:
-                l=[]
-                l.append(value)
-                l.append(b[1])
-                d[key]=l
-        else:
-            print("error: time-to-live of",key,"has expired") #error message5
-    else:
-        if key not in d:
-            print("error: given key does not exist in database. Please enter a valid key") #error message6
-        else:
-            l=[]
-            l.append(value)
-            l.append(b[1])
-            d[key]=l
-
+            del datastore[key]
+            print("Key deleted")
